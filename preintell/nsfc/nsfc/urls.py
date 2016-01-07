d0abf7c135f771eb3828b9284aa622bd52ea3ac1
Hao Jing <hao.jing@tjut.cc>
2016-01-07 11:18:19 +0800

    web home

--- preintell/nsfc/nsfc/urls.py
+++ preintell/nsfc/nsfc/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('project.views',
-    # Examples:
-    # url(r'^$', 'nsfc.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^project/$', 'my_view'),
